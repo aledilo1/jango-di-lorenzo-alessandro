e195af3e24ac7fe5f517b6db96319026a01230bd
aledilo1 <ale.2788dilore@gmail.com>
2021-11-16 08:33:33 +0100

    commit seconda_applicazione

--- seconda_app/apps.py
+++ seconda_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SecondaApplicazioneConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seconda_applicazione'
+    name = 'seconda_app'
